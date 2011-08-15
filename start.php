7ff82023ebef88722ffbba928e7f18aa654e7ecd
Hans Duedal <hd@onlinecity.dk>
2011-08-15 11:33:33 +0200

    Added PID to logs

--- start.php
+++ start.php
@@ -7,14 +7,14 @@ require_once 'factory.class.php';
 
 function debug($string) {
 	list($microtime, $time) = explode(' ',microtime());
-	$prefix = sprintf("%s.%02.2s ",date('Y-m-d\TH:i:s'),(int) ($microtime*1000000));
+	$prefix = sprintf("%s.%02.2s P:%05s ",date('Y-m-d\TH:i:s'),(int) ($microtime*1000000),getmypid());
 	$s = $prefix.str_replace("\n","\n".$prefix."\t",trim($string))."\n";
 	file_put_contents('debug.log',$s,FILE_APPEND);
 }
 
 function protocolDebug($string) {
 	list($microtime, $time) = explode(' ',microtime());
-	$prefix = sprintf("%s.%02.2s ",date('Y-m-d\TH:i:s'),(int) ($microtime*1000000));
+	$prefix = sprintf("%s.%02.2s P:%05s ",date('Y-m-d\TH:i:s'),(int) ($microtime*1000000),getmypid());
 	$s = $prefix.str_replace("\n","\n".$prefix."\t",trim($string))."\n";
 	file_put_contents('trace.log',$s,FILE_APPEND);
 }
