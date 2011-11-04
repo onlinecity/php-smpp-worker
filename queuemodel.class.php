0d586d89c7b12d180523880baadfc5b0cc090951
duedal <hd@onlinecity.dk>
2011-11-04 22:37:26 +0100

    Fixed notices about php-redis returning non-parsed null

--- queuemodel.class.php
+++ queuemodel.class.php
@@ -66,7 +66,7 @@ class QueueModel
 	public function consume($pid,$timeout=5)
 	{
 		$m = $this->redis->brpoplpush($this->key.':inactive',$this->key.':active:'.$pid,$timeout);
-		if ($m === false) return $m;
+		if (is_null($m) || $m == '*-1') return null;
 		return $this->unserialize($m);
 	}
 	
@@ -159,6 +159,7 @@ class QueueModel
 	public function lastDeferred()
 	{
 		$m = $this->redis->lIndex($this->key.':deferred',-1);
+		if (is_null($m) || $m == '*-1') return null;
 		return $this->unserialize($m);
 	}
 	
